5ee31dd0cbd3e52f432fc9d31a14b34bf6ddb21c
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-06-29 23:29:39 -0700

    completed

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -40,7 +40,7 @@ class ContactForm extends Component {
               name="name"
               value={this.state.name}
               onChange={this.handleChange}
-              // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
               title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
               id={this.nameInput}
               required
@@ -53,7 +53,7 @@ class ContactForm extends Component {
               name="number"
               value={this.state.number}
               onChange={this.handleChange}
-              // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
               title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
               id={this.phoneInput}
               required
